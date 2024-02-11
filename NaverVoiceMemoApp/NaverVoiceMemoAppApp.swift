a1064216de5b03a64478f6dde4be1e47e9713114
dhk767 <dhk767@gmail.com>
2024-02-11 21:43:28 +0900

    Initial Commit

---
+++ NaverVoiceMemoApp/NaverVoiceMemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NaverVoiceMemoAppApp.swift
+//  NaverVoiceMemoApp
+//
+//  Created by 김동현 on 2/11/24.
+//
+
+import SwiftUI
+
+@main
+struct NaverVoiceMemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
